c0037033c5ef84e7e414b56138aa6273a6f39773
tral909 <indorm1992@mail.ru>
2023-06-20 23:36:26 +0300

    Form Layout

--- src/pages/UserSignupPage.spec.js
+++ src/pages/UserSignupPage.spec.js
@@ -1,15 +1,51 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { cleanup, render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { UserSignupPage } from './UserSignupPage'
 
+//beforeEach(cleanup)
+
 describe('UserSignupPage', () => {
-    describe('Layout', () => {
-        
+    describe('Layout', () => { 
         it('has header of Sign Up', () => {
             const { container } = render(<UserSignupPage />)
             const header = container.querySelector('h1')
             expect(header).toHaveTextContent('Sign Up')
+        })
+        it('has input for display name', () => {
+            const { queryByPlaceholderText } = render(<UserSignupPage />)
+            const displayNameInput = queryByPlaceholderText('Your display name')
+            expect(displayNameInput).toBeInTheDocument()
+        })
+        it('has input for username', () => {
+            const { queryByPlaceholderText } = render(<UserSignupPage />)
+            const usernameInput = queryByPlaceholderText('Your username')
+            expect(usernameInput).toBeInTheDocument()
+        })
+        it('has input for password', () => {
+            const { queryByPlaceholderText } = render(<UserSignupPage />)
+            const passwordInput = queryByPlaceholderText('Your password')
+            expect(passwordInput).toBeInTheDocument()
+        })
+        it('has password type for password input', () => {
+            const { queryByPlaceholderText } = render(<UserSignupPage />)
+            const passwordInput = queryByPlaceholderText('Your password')
+            expect(passwordInput.type).toBe('password')
+        })
+        it('has input for password repeate', () => {
+            const { queryByPlaceholderText } = render(<UserSignupPage />)
+            const passwordRepeat = queryByPlaceholderText('Repeat your password')
+            expect(passwordRepeat).toBeInTheDocument()
+        })
+        it('has password type for password repeat input', () => {
+            const { queryByPlaceholderText } = render(<UserSignupPage />)
+            const passwordRepeat = queryByPlaceholderText('Your password')
+            expect(passwordRepeat.type).toBe('password')
+        })
+        it('has submit button', () => {
+            const { container } = render(<UserSignupPage />)
+            const button = container.querySelector('button')
+            expect(button).toBeInTheDocument()
         })
     })
 })
