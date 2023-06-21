01508bff6c383de63c453f74b056e57c10b24726
tral909 <indorm1992@mail.ru>
2023-06-22 00:18:31 +0300

    Styling

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -40,36 +40,45 @@ export class UserSignupPage extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Sign Up</h1>
-                <div>
+            <div className='container'>
+                <h1 className='text-center'>Sign Up</h1>
+                <div className='col-12 mb-3'>
+                    <label className='mb-1'>Display name</label>
                     <input
+                        className='form-control'
                         placeholder='Your display name'
                         value={this.state.displayName}
                         onChange={this.onChangeDisplayName} />
                 </div>
-                <div>
+                <div className='col-12 mb-3'>
+                    <label className='mb-1'>Username</label>
                     <input
+                        className='form-control'
                         placeholder='Your username'
                         value={this.state.username}
                         onChange={this.onChangeUsername} />
                 </div>
-                <div>
+                <div className='col-12 mb-3'>
+                    <label className='mb-1'>Password</label>
                     <input
+                        className='form-control'
                         placeholder='Your password'
                         type='password'
                         value={this.state.password}
                         onChange={this.onChangePassword} />
                 </div>
-                <div>
+                <div className='col-12 mb-3'>
+                    <label className='mb-1'>Password repeat</label>
                     <input
+                        className='form-control'
                         placeholder='Repeat your password'
                         type='password'
                         value={this.state.passwordRepeat}
                         onChange={this.onChangePasswordRepeat} />
                 </div>
-                <div>
+                <div className='text-center'>
                     <button
+                        className='btn btn-primary'
                         onClick={this.onClickSignup}
                     >Sign Up</button>
                 </div>
