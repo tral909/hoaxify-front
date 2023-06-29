3f773bacfc9c5267369b471e7b5f840c3ee4eea1
tral909 <indorm1992@mail.ru>
2023-06-29 08:23:52 +0300

    Handling Input Change

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,15 +3,35 @@ import Input from '../components/Input'
 
 export class LoginPage extends React.Component {
 
+    state = {
+        username: '',
+        password: ''
+    }
+
+    onChangeUsername = (event) => {
+        const value = event.target.value
+        this.setState({
+            username: value
+        })
+    }
+
     render() {
         return (
             <div className='container'>
                 <h1 className='text-center'>Login</h1>
                 <div className='col-12 mb-3'>
-                    <Input label='Username' placeholder='Your username' />
+                    <Input
+                        label='Username'
+                        placeholder='Your username'
+                        value={this.state.username}
+                        onChange={this.onChangeUsername} />
                 </div>
                 <div className='col-12 mb-3'>
-                    <Input label='Password' type='password' placeholder='Your password' />
+                    <Input
+                        label='Password'
+                        type='password'
+                        placeholder='Your password'
+                        onChange={(ev) => this.setState({ password: ev.target.value })} />
                 </div>
                 <div className='text-center'>
                     <button className='btn btn-primary'>Login</button>
--- src/pages/LoginPage.spec.js
+++ src/pages/LoginPage.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import { LoginPage } from './LoginPage'
 
 describe('LoginPage', () => {
@@ -12,8 +12,8 @@ describe('LoginPage', () => {
 
         it('has input for username', () => {
             const { queryByPlaceholderText } = render(<LoginPage />)
-            const input = queryByPlaceholderText('Your username')
-            expect(input).toBeInTheDocument()
+            const usernameInput = queryByPlaceholderText('Your username')
+            expect(usernameInput).toBeInTheDocument()
         })
 
         it('has input for password', () => {
@@ -32,6 +32,29 @@ describe('LoginPage', () => {
             const { container } = render(<LoginPage />)
             const button = container.querySelector('button')
             expect(button).toBeInTheDocument()
+        })
+    })
+    describe('Interactions', () => {
+        const changeEvent = (content) => {
+            return {
+                target: {
+                    value: content
+                }
+            }
+        }
+        
+        it('sets the username value into state', () => {
+            const { queryByPlaceholderText } = render(<LoginPage />)
+            const usernameInput = queryByPlaceholderText('Your username')
+            fireEvent.change(usernameInput, changeEvent('my-user-name'))
+            expect(usernameInput).toHaveValue('my-user-name')
+        })
+
+        it('sets the password value into state', () => {
+            const { queryByPlaceholderText } = render(<LoginPage />)
+            const passwordInput = queryByPlaceholderText('Your password')
+            fireEvent.change(passwordInput, changeEvent('P4ssword'))
+            expect(passwordInput).toHaveValue('P4ssword')
         })
     })
 })
