37c0822fd1ac8beba071e6a78f99607187419dee
tral909 <indorm1992@mail.ru>
2023-09-18 10:14:10 +0300

    TopBar with User Info

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../assets/hoaxify-logo.png'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import ProfileImageWithDefault from './ProfileImageWithDefault'
 
 class TopBar extends React.Component {
 
@@ -30,6 +31,15 @@ class TopBar extends React.Component {
         if (this.props.user.isLoggedIn) {
             links = (
                 <ul className='nav navbar-nav ml-auto'>
+                    <li className='nav-item nav-link'>
+                        <ProfileImageWithDefault
+                            image={this.props.user.image}
+                            className='rounded-circle'
+                            width='32'
+                            height='32'
+                        />
+                        {this.props.user.displayName}
+                    </li>
                     <li className='nav-item nav-link' onClick={this.onClickLogout} style={{
                         cursor: 'pointer'
                     }}>Logout</li>
--- src/components/TopBar.spec.js
+++ src/components/TopBar.spec.js
@@ -73,6 +73,19 @@ describe('TopBar', () => {
             const profileLink = queryByText('My Profile')
             expect(profileLink.getAttribute('href')).toBe('/user1')
         })
+
+        it('displays the displayName when user logged in', () => {
+            const { queryByText } = setup(loggedInState)
+            const displayName = queryByText('display1')
+            expect(displayName).toBeInTheDocument()
+        })
+
+        it('displays user image when user logged in', () => {
+            const { container } = setup(loggedInState)
+            const images = container.querySelectorAll('img')
+            const userImage = images[1]
+            expect(userImage.src).toContain('/images/profile/' + loggedInState.image)
+        })
     })
 
     describe('Interactions', () => {
