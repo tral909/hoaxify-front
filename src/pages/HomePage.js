2c8f489c4841212b98abb98e46979ddd8a168162
tral909 <indorm1992@mail.ru>
2023-09-27 00:03:49 +0300

    Hoax Submit View

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import UserList from '../components/UserList'
+import HoaxSubmit from '../components/HoaxSubmit'
 
 export class HomePage extends React.Component {
     render() {
         return <div data-testid='homepage'>
-            <UserList />
+            <div className='row'>
+                <div className='col-8'>
+                    <HoaxSubmit />
+                </div>
+                <div className='col-4'>
+                    <UserList />
+                </div>
+            </div>
         </div>
     }
 }
