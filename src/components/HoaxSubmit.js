b69dd2fd648a1036f922485ac6da18f0154dd89f
tral909 <indorm1992@mail.ru>
2023-09-28 15:16:41 +0300

    Connecting Hoax Submit View to Redux

--- src/components/HoaxSubmit.js
+++ src/components/HoaxSubmit.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
+import { connect } from 'react-redux'
 
-class HoaxSubmit extends Component {
+class HoaxSubmit extends React.Component {
     render() {
         return (
             <div className='card d-flex flex-row p-1'>
@@ -9,6 +10,7 @@ class HoaxSubmit extends Component {
                     className='rounded-circle m-1'
                     width='32'
                     height='32'
+                    image={this.props.loggedInUser.image}
                 />
                 <div className='flex-fill'>
                     <textarea className='form-control w-100' rows={1} />
@@ -18,4 +20,10 @@ class HoaxSubmit extends Component {
     }
 }
 
-export default HoaxSubmit;
+const mapStateToProps = (state) => {
+    return {
+        loggedInUser: state
+    }
+}
+
+export default connect(mapStateToProps)(HoaxSubmit);
--- src/pages/HomePage.spec.js
+++ src/pages/HomePage.spec.js
@@ -2,6 +2,29 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { HomePage } from './HomePage'
 import * as apiCalls from '../api/apiCalls'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import authReducer from '../redux/authReducer'
+
+const defaultState = {
+    id: 1,
+    username: 'user1',
+    displayName: 'display1',
+    image: 'profile.png',
+    password: 'P4ssword',
+    isLoggedIn: true
+}
+
+let store
+
+const setup = (state = defaultState) => {
+    store = createStore(authReducer, state)
+    return render(
+        <Provider store={store}>
+            <HomePage />
+        </Provider>
+    )
+}
 
 beforeEach(() => {
     apiCalls.listUsers = jest.fn().mockResolvedValue({
@@ -17,7 +40,7 @@ describe('HomePage', () => {
 
     describe('Layout', () => {
         it('has root page dev', () => {
-            const { queryByTestId } = render(<HomePage />)
+            const { queryByTestId } = setup()
             const homePageDiv = queryByTestId('homepage')
             expect(homePageDiv).toBeInTheDocument()
         })
